c90eb44f46ab8233cf3d1103f5eb028b36162a3b
Gerasin Ivan <webartmotor@gmail.com>
2019-12-20 15:00:08 +0300

    build: setting commitezen, commitlint, pretier and tslint
    use command 'yarn commit' for commit
    use command 'yarn tslint' for linting

---
+++ commitizen.js
@@ -0,0 +1,33 @@
+module.exports = {
+  types: [
+    { value: 'feat', name: 'feat: Adding new functionality' },
+    { value: 'fix', name: 'fix: Fix bugs' },
+    { value: 'test', name: 'test: Build test' },
+    { value: 'build', name: 'build: Build project or change dependencies' },
+    { value: 'perf', name: 'perf: Performance improvement' },
+    {
+      value: 'refactor',
+      name: 'refactor: Editing code without fixing bugs or adding new features',
+    },
+    { value: 'revert', name: 'revert: Rollback to previous commits' },
+    {
+      value: 'style',
+      name: 'style: Edits by code style (tabs, indents, periods, commas ...)',
+    },
+  ],
+  scopes: [],
+  messages: {
+    type: 'What changes are you making?',
+    scope: 'Select the scope that you changed (optional):',
+    customScope: 'Insert your scope:',
+    subject: 'Write a short description:',
+    body: 'Write a detailed description (optional). Use "|" for a new line:',
+    breaking: 'List BREAKING CHANGES (optional):',
+    footer: 'Place for meta data (tasks, links):',
+    confirmCommit: 'You are satisfied with the resulting commit?',
+  },
+  allowCustomScopes: true,
+  allowBreakingChanges: false,
+  footerPrefix: '#42343:',
+  subjectLimit: 80,
+};
